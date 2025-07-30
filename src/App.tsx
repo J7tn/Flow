c55befeb464d3d224ab8416fc524903e13657c0e
j7tn <jntnnn4@gmail.com>
2025-07-30 08:23:56 +0000

    df

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { Suspense } from "react";
 import { useRoutes } from "react-router-dom";
 import Home from "./components/home";
+import Calendar from "./components/Calendar";
+import Settings from "./components/Settings";
+import MyWorkflows from "./components/MyWorkflows";
+import FlowTemplates from "./components/FlowTemplates";
+import Analytics from "./components/Analytics";
 import routes from "tempo-routes";
 
 function App() {
@@ -8,6 +13,26 @@ function App() {
     {
       path: "/",
       element: <Home />,
+    },
+    {
+      path: "/calendar",
+      element: <Calendar />,
+    },
+    {
+      path: "/settings",
+      element: <Settings />,
+    },
+    {
+      path: "/workflows",
+      element: <MyWorkflows />,
+    },
+    {
+      path: "/templates",
+      element: <FlowTemplates />,
+    },
+    {
+      path: "/analytics",
+      element: <Analytics />,
     },
   ];
 
@@ -18,11 +43,7 @@ function App() {
 
   const element = useRoutes(allRoutes);
 
-  return (
-    <Suspense fallback={<p>Loading...</p>}>
-      {element}
-    </Suspense>
-  );
+  return <Suspense fallback={<p>Loading...</p>}>{element}</Suspense>;
 }
 
 export default App;
